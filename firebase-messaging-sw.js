21e55f2b045b95186a97f0490dc420d0015b0c4e
Manisha-athili <manishaathili1126@gmail.com>
2025-03-22 20:50:26 +0530

    import statements corrected

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging.js');
+// Import Firebase libraries
+import firebase from 'https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js';
+import 'https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging.js';
 
 firebase.initializeApp({
     apiKey: "AIzaSyBsJS-phFoPIzSEtjdr0Y9lZ-J79XpKjV8",
