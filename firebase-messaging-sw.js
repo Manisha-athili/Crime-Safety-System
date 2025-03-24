14980e7ee45164abbfb67cca6917d7ef84c01d52
Manisha-athili <manishaathili1126@gmail.com>
2025-03-24 12:42:12 +0530

    some

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,19 +2,23 @@
 import firebase from 'https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js';
 import 'https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging.js';
 import 'firebase/messaging';
+import { getMessaging } from 'https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging.js';
 
 
-firebase.initializeApp({
-    apiKey: "AIzaSyBsJS-phFoPIzSEtjdr0Y9lZ-J79XpKjV8",
-    authDomain: "crime-and-safety.firebaseapp.com",
-    projectId: "crime-and-safety",
-    storageBucket: "crime-and-safety.appspot.com",
-    messagingSenderId: "167045576983",
-    appId: "1:167045576983:web:4b19f62d9f0268af565f52",
-    measurementId: "G-QRL88H2M6Q"
-});
+// 🚀 Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBsJS-phFoPIzSEtjdr0Y9lZ-J79XpKjV8",
+  authDomain: "crime-and-safety.firebaseapp.com",
+  projectId: "crime-and-safety",
+  storageBucket: "crime-and-safety.appspot.com",
+  messagingSenderId: "167045576983",
+  appId: "1:167045576983:web:4b19f62d9f0268af565f52",
+  measurementId: "G-QRL88H2M6Q"
+};
 
-const messaging = firebase.messaging();
+const app = initializeApp(firebaseConfig);
+// const messaging = firebase.messaging();
+const messaging = getMessaging(app);
 
 
 // Handle background notifications
