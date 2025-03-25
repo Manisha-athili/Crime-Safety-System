f2138d51c314126cdc00b29142483a0a0b3757a8
Manisha-athili <manishaathili1126@gmail.com>
2025-03-25 10:18:22 +0530

    d

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ const messaging = getMessaging(app);
 if ("serviceWorker" in navigator) {
         console.log("coming into if");
         
-    navigator.serviceWorker.register("firebase-messaging-sw.js", { scope: "/" }).then(
+    navigator.serviceWorker.register("/firebase-messaging-sw.js")
+    .then(
       (registration) => {
         console.log("success");
         
