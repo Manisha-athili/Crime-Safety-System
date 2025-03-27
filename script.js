4e3c6e700c9498ab30e310f6fae2860e32ccb3f3
Manisha-athili <manishaathili1126@gmail.com>
2025-03-27 15:16:13 +0530

    .

--- script.js
+++ script.js
@@ -41,8 +41,7 @@ const initializeMessaging = async () => {
 if ('serviceWorker' in navigator) {
     (async () => {
         try {
-            const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js'
-            );
+            const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
             console.log('Service Worker registered with scope:', registration.scope);
         } catch (error) {
             console.error('Service Worker registration failed:', error);
